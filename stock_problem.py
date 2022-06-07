a6451ffc35cf306a82ad0e5c49b21439368810e2
Wilton Santhosh D Souza <wiltondsouza100@gmail.com>
2022-06-07 15:24:29 +0530

    Updated code with input file

--- stock_problem.py
+++ stock_problem.py
@@ -1,37 +1,123 @@
-import math
-def maxProfitSUBARRAY(A, low, mid, high):
-    left_sum = float('-inf')
-    sum = 0
-    max_left = 0
-    max_right = 0
-    for i in range(mid, low-1 ,-1):
-        sum = sum + A[i]
-        if sum > left_sum:
-            left_sum = sum
-            max_left = i
-    right_sum = float('-inf')
-    sum = 0
-    for j in range(mid+1, high+1):
-        sum = sum + A[j]
-        if sum > right_sum:
-            right_sum = sum
-            max_right = j
-    return max_left, max_right, left_sum + right_sum
 
-def maxProfit(A, low, high):
-    if high == low:
-        return (low, high, A[low])
-    else: 
-        mid = math.floor((low + high) / 2)
-        left_low, left_high, left_sum = maxProfit(A,low, mid)
-        right_low, right_high, right_sum = maxProfit(A, mid+1, high)
-        cross_low, cross_high, cross_sum = maxProfitSUBARRAY(A, low, mid, high)
-        if left_sum >= right_sum and left_sum >= cross_sum:
-            return left_low, left_high, left_sum
-        elif right_sum >= left_sum and right_sum >= cross_sum:
-            return right_low, right_high, right_sum
-        else:
-            return cross_low, cross_high, cross_sum
-price =  [-5, 8, 9, -6, 10, -15, 3]
-n = len(price)
-print(maxProfit(price, 0, n-1))
+def maxCrossingSum(arr, l, m, h):
+
+	# Include elements on left of mid.
+	sm = 0
+	left_sum = -10000
+
+	for i in range(m, l-1, -1):
+		sm = sm + arr[i]
+
+		if (sm > left_sum):
+			left_sum = sm
+
+	# Include elements on right of mid
+	sm = 0
+	right_sum = -1000
+	for i in range(m + 1, h + 1):
+		sm = sm + arr[i]
+
+		if (sm > right_sum):
+			right_sum = sm
+
+	# Return sum of elements on left and right of mid
+	# returning only left_sum + right_sum will fail for [-2, 1]
+	return max(left_sum + right_sum, left_sum, right_sum)
+
+
+# Returns sum of maximum sum subarray in aa[l..h]
+def maxSubArraySum(arr, l, h):
+
+	# Base Case: Only one element
+	if (l == h):
+		return arr[l]
+
+	# Find middle point
+	m = (l + h) // 2
+
+	# Return maximum of following three possible cases
+	# a) Maximum subarray sum in left half
+	# b) Maximum subarray sum in right half
+	# c) Maximum subarray sum such that the
+	#	 subarray crosses the midpoint
+	return max(maxSubArraySum(arr, l, m),
+			maxSubArraySum(arr, m+1, h),
+			maxCrossingSum(arr, l, m, h))
+
+
+
+def KadaneSum(a,size):
+	
+	max_so_far = -10000
+	max_ending_here = 0
+	
+	for i in range(0, size):
+		max_ending_here = max_ending_here + a[i]
+		if (max_so_far < max_ending_here):
+			max_so_far = max_ending_here
+
+		if max_ending_here < 0:
+			max_ending_here = 0
+	return max_so_far
+
+
+def subArraySum(arr, n, sum_):
+	
+	# Pick a starting
+	# point
+	for i in range(n):
+		curr_sum = arr[i]
+	
+		# try all subarrays
+		# starting with 'i'
+		j = i + 1
+		f1 = open("outputPS5.txt", "a")
+		while j <= n:
+		
+			if curr_sum == sum_:
+				str3 = "Day to buy:" + str(i+1) + "\nDay to sell:"+ str(j-1+2)+"\n\n"
+				f1.write(str3)
+				f1.close
+				return 1
+				
+			if curr_sum > sum_ or j == n:
+				break
+			
+			curr_sum = curr_sum + arr[j]
+			j += 1
+
+	print ("No subarray found")
+	return 0
+
+fp = open("inputPS5.txt", "r")
+f =open("outputPS5.txt", 'r+') 
+f.truncate(0)
+f.close()
+day_number=0
+price=[]
+while True:
+    lines = fp.readline()
+    if lines == "":
+        break
+    day_number=day_number+1
+    price.append(int(lines.split("/")[1]))
+fp.close()
+res = [price[i + 1] - price[i] for i in range(len(price)-1)]
+n = len(res)
+f = open("outputPS5.txt", "a")
+max_sum = maxSubArraySum(res, 0, n-1)
+str1="Maximum Profit (Divide & Conquer solution): "+ str(max_sum)+"\n"
+f.write(str1)
+f.close()
+subArraySum(res, n, max_sum)
+max_sum2=KadaneSum(res,n)
+str2="Maximum Profit (Iterative solution): "+ str(max_sum2)+"\n"
+f = open("outputPS5.txt", "a")
+f.write(str2)
+f.close()
+subArraySum(res, n, max_sum2)
+
+
+# Time Complexity for divide and conquer: maxSubArraySum() is a recursive method and time complexity can be expressed as following recurrence relation. 
+# T(n) = 2T(n/2) + Θ(n) 
+# Time Complexity : O(nlogn)
