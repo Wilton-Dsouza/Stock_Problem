81cf0ffdf620bbad686fe9a800b7edb526db4595
Wilton Santhosh D Souza <wiltondsouza100@gmail.com>
2022-06-09 17:49:07 +0530

    added commits and the input file

--- bitcoin.py
+++ bitcoin.py
@@ -1,88 +1,85 @@
-def maxCrossingSum(A, low, mid, high):
-	sum = 0
-	left_sum = -10000
+def find_max_crossing_subarray(a,low,mid,high): 
+	# compute the max sum from left half and right half of the array 
+	# and compare it with the sum of left and right halves together 
+	# and return the overall maximum value.
+	lsum=-10000
+	sum=0
 	for i in range(mid, low-1, -1):
-		sum = sum + A[i]
-		if (sum > left_sum):
-			left_sum = sum
+		sum = sum + a[i]
+
+		if (sum > lsum):
+			lsum = sum
+			maxleft=i
 	sum = 0
-	right_sum = -1000
-	for i in range(mid + 1, high + 1):
-		sum = sum + A[i]
-		if (sum > right_sum):
-			right_sum = sum
-	return max(left_sum + right_sum, left_sum, right_sum)
+	rsum = -10000
+	for j in range(mid + 1, high + 1):
+		sum = sum + a[j]
 
-def maxSubArraySum(A, low, high):
-	if (low == high):
-		return A[low]
-	mid = (low + high) // 2
-	return max(maxSubArraySum(A, low, mid),
-			maxSubArraySum(A, mid+1, high),
-			maxCrossingSum(A, low, mid, high))
+		if (sum > rsum):
+			rsum = sum
+			maxright=j
+	return (maxleft,maxright,lsum+rsum)
 
-
+def find_maximum_subarray(a,low,high):
+	# takes the array with its first and last index and 
+	# checks if the array has elements or not. 
+	if high == low:
+		return (low,high,a[low])
+	else:
+		mid=(low+high)//2
+		llow,lhigh,lsum=find_maximum_subarray(a,low,mid)
+		rlow,rhigh,rsum=find_maximum_subarray(a,mid+1,high)
+		clow,chigh,csum=find_max_crossing_subarray(a,low,mid,high)
+	if lsum>=rsum and lsum>=csum:
+		return (llow,lhigh,lsum)
+	elif rsum>=lsum and rsum>=csum:
+		return (rlow,rhigh,rsum)
+	else:
+		return (clow,chigh,csum)
 
 def IterativeSum(a):
-    maxprofit = 0
-    minsofar = a[0]
-    for i in range(0,len(a)):
-        minsofar = min(minsofar,a[i])
-        profit = a[i]-minsofar
-        maxprofit = max(maxprofit,profit)
-    return maxprofit
-
-
-def subArraySum(A, A_len, maxsum):
-
-	f1 = open("outputPS5.txt", "a")
-	if maxsum<=0:
-		str4="Day to buy: 1\nDay to sell: 1\n\n"
-		f1.write(str4)
-		return 1
-	for i in range(A_len):
-		curr_sum = A[i]
-		j = i + 1
-		while j <= n:
-			if curr_sum == maxsum:
-				str3 = "Day to buy:" + str(i+1) + "\nDay to sell:"+ str(j-1+2)+"\n\n"
-				f1.write(str3)
-				f1.close()
-				return 1	
-			if curr_sum > maxsum or j == n:
-				break
-			curr_sum = curr_sum + A[j]
-			j += 1
-	return 0
+	# iterative approach to find the maximum profit with the start and the end day.
+	maxprofit = 0
+	minsofar = a[0]
+	st = 0
+	start = 0
+	end =0
+	for i in range(0,len(a)):
+		if a[i]<=minsofar:
+			st = i
+		minsofar = min(minsofar,a[i])
+		profit = a[i]-minsofar
+		if maxprofit<profit:
+			end = i
+			start = st
+		maxprofit = max(maxprofit,profit)
+		
+	return (start,end,maxprofit)
 
 fp = open("inputPS5.txt", "r")
-day_number=0
 price=[]
 while True:
     lines = fp.readline()
     if lines == "":
         break
-    day_number=day_number+1
     price.append(int(lines.split("/")[1]))
 fp.close()
 res = [price[i + 1] - price[i] for i in range(len(price)-1)]
 n = len(res)
 f = open("outputPS5.txt", "w")
-max_sum = maxSubArraySum(res, 0, n-1)
-if max_sum<0:
-	str1="Maximum Profit (Divide & Conquer solution): 0\n"
+day_buy,day_sell,max_profit = find_maximum_subarray(res, 0, n-1)
+if max_profit<=0:
+	str1="Maximum Profit (Divide & Conquer solution): 0 \nDay to buy:0 \nDay to sell:0\n"
 else:
-	str1="Maximum Profit (Divide & Conquer solution): "+ str(max_sum)+"\n"
+	str1="Maximum Profit (Divide & Conquer solution): "+ str(max_profit)+"\n"+"Day to buy:"+str(day_buy+1)+"\nDay to sell:"+str(day_sell+2)+"\n"
 f.write(str1)
 f.close()
-subArraySum(res, n, max_sum)
-max_sum2=IterativeSum(price)
-str2="Maximum Profit (Iterative solution): "+ str(max_sum2)+"\n"
+
+day_buy2,day_sell2,max_profit2=IterativeSum(price)
+if max_profit2<=0:
+	str2="Maximum Profit (Iterative solution): 0 \nDay to buy:0 \nDay to sell:0\n"
+else:
+	str2="Maximum Profit (Iterative solution): "+ str(max_profit2)+"\n"+"Day to buy:"+str(day_buy2+1)+"\nDay to sell:"+str(day_sell2+1)+"\n"
 f = open("outputPS5.txt", "a")
 f.write(str2)
 f.close()
-subArraySum(res, n, max_sum2)
-
-# Time Complexity for divide and conquer: maxSubArraySum() is a recursive method and time complexity can be expressed as following recurrence relation. 
-# T(n) = 2T(n/2) + Θ(n) 
-# Time Complexity : O(nlogn)
