6cc2629a6d8f2eb4ddcca070cecc6896e875bce6
Wilton Santhosh D Souza <wiltondsouza100@gmail.com>
2022-06-10 17:53:42 +0530

    Final Code

--- bitcoin.py
+++ bitcoin.py
@@ -1,7 +1,6 @@
 def find_max_crossing_subarray(a,low,mid,high): 
 	# compute the max sum from left half and right half of the array 
-	# and compare it with the sum of left and right halves together 
-	# and return the overall maximum value.
+	# and return the overall maximum value with maxleft and max right indexes.
 	lsum=-10000
 	sum=0
 	for i in range(mid, low-1, -1):
@@ -22,14 +21,14 @@ def find_max_crossing_subarray(a,low,mid,high):
 
 def find_maximum_subarray(a,low,high):
 	# takes the array with its first and last index and 
-	# checks if the array has elements or not. 
+	# if the array has only one value, it will return the index and value of it. 
 	if high == low:
 		return (low,high,a[low])
 	else:
-		mid=(low+high)//2
-		llow,lhigh,lsum=find_maximum_subarray(a,low,mid)
-		rlow,rhigh,rsum=find_maximum_subarray(a,mid+1,high)
-		clow,chigh,csum=find_max_crossing_subarray(a,low,mid,high)
+		mid=(low+high)//2 
+		llow,lhigh,lsum=find_maximum_subarray(a,low,mid)  # for the first half of the array
+		rlow,rhigh,rsum=find_maximum_subarray(a,mid+1,high)  # for the second half of the array
+		clow,chigh,csum=find_max_crossing_subarray(a,low,mid,high) # for maximum subarray computation
 	if lsum>=rsum and lsum>=csum:
 		return (llow,lhigh,lsum)
 	elif rsum>=lsum and rsum>=csum:
@@ -41,45 +40,52 @@ def IterativeSum(a):
 	# iterative approach to find the maximum profit with the start and the end day.
 	maxprofit = 0
 	minsofar = a[0]
-	st = 0
+	min_start = 0
 	start = 0
 	end =0
 	for i in range(0,len(a)):
 		if a[i]<=minsofar:
-			st = i
+			min_start = i
 		minsofar = min(minsofar,a[i])
 		profit = a[i]-minsofar
 		if maxprofit<profit:
 			end = i
-			start = st
+			start = min_start
 		maxprofit = max(maxprofit,profit)
 		
 	return (start,end,maxprofit)
 
 fp = open("inputPS5.txt", "r")
 price=[]
+days=[]
 while True:
-    lines = fp.readline()
-    if lines == "":
-        break
-    price.append(int(lines.split("/")[1]))
+	lines = fp.readline()
+	if lines == "":
+		break
+	price.append(int(lines.split("/")[1]))
+	days.append(int(lines.split("/")[0]))
 fp.close()
-res = [price[i + 1] - price[i] for i in range(len(price)-1)]
-n = len(res)
 f = open("outputPS5.txt", "w")
-day_buy,day_sell,max_profit = find_maximum_subarray(res, 0, n-1)
-if max_profit<=0:
-	str1="Maximum Profit (Divide & Conquer solution): 0 \nDay to buy:0 \nDay to sell:0\n"
+day = days[0]
+if len(price) == 1:
+	str1="Maximum Profit (Divide & Conquer solution): 0 \nDay to buy:"+str(day)+" \nDay to sell:"+str(day)+"\n"
+	f.write(str1)
+	str2="Maximum Profit (Iterative solution): 0 \nDay to buy:"+str(day)+" \nDay to sell:"+str(day)+"\n"
+	f.write(str2)
 else:
-	str1="Maximum Profit (Divide & Conquer solution): "+ str(max_profit)+"\n"+"Day to buy:"+str(day_buy+1)+"\nDay to sell:"+str(day_sell+2)+"\n"
-f.write(str1)
-f.close()
-
-day_buy2,day_sell2,max_profit2=IterativeSum(price)
-if max_profit2<=0:
-	str2="Maximum Profit (Iterative solution): 0 \nDay to buy:0 \nDay to sell:0\n"
-else:
-	str2="Maximum Profit (Iterative solution): "+ str(max_profit2)+"\n"+"Day to buy:"+str(day_buy2+1)+"\nDay to sell:"+str(day_sell2+1)+"\n"
-f = open("outputPS5.txt", "a")
-f.write(str2)
+	res = [price[i + 1] - price[i] for i in range(len(price)-1)]
+	n = len(res)
+	day_buy,day_sell,max_profit = find_maximum_subarray(res, 0, n-1)
+	if max_profit<=0:
+		str1="Maximum Profit (Divide & Conquer solution): 0 \nDay to buy:"+str(day)+" \nDay to sell:"+str(day)+"\n"
+	else:
+		str1="Maximum Profit (Divide & Conquer solution): "+ str(max_profit)+"\n"+"Day to buy:"+str(day_buy+day)+"\nDay to sell:"+str(day_sell+day+1)+"\n"
+	f.write(str1)
+	day_buy2,day_sell2,max_profit2=IterativeSum(price)
+	if max_profit2<=0:
+		str2="Maximum Profit (Iterative solution): 0 \nDay to buy:"+str(day)+" \nDay to sell:"+str(day)+"\n"
+	else:
+		str2="Maximum Profit (Iterative solution): "+ str(max_profit2)+"\n"+"Day to buy:"+str(day_buy2+day)+"\nDay to sell:"+str(day_sell2+day)+"\n"
+	f = open("outputPS5.txt", "a")
+	f.write(str2)
 f.close()
